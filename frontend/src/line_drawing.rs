9d46b2d79c500c196b58882561ea5856840d09b9
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-06-29 18:56:23 +0200

    Fix arc drawing

--- frontend/src/line_drawing.rs
+++ frontend/src/line_drawing.rs
@@ -1,10 +1,11 @@
 use itertools::Itertools;
 use leptos::html::li;
-use std::f64::consts::{FRAC_PI_2, PI};
+use std::f64::consts::{FRAC_PI_2, TAU};
 
 use nalgebra::{ComplexField, Matrix2, Point2, Rotation, Rotation2, Vector2};
 
 type Point = Point2<f64>;
+type Vector = Vector2<f64>;
 
 pub fn line_into_triangles(line: Vec<Point>, width: f64) -> Vec<Point> {
     if let [a] = line[..] {
@@ -37,6 +38,7 @@ fn rectangle(from: Point, to: Point, width: f64) -> Vec<Point> {
 }
 
 const ANGLE_RES: f64 = 0.3;
+const MIN_ANGLE: f64 = 0.001;
 
 fn cap(from: Point, to: Point, width: f64) -> Vec<Point> {
     let dir = (to - from).normalize();
@@ -49,11 +51,11 @@ fn cap(from: Point, to: Point, width: f64) -> Vec<Point> {
 }
 
 fn circle(a: Point, width: f64) -> Vec<Point> {
-    let no_segments: u32 = (2.0 * PI / ANGLE_RES).ceil() as u32;
+    let segment_count: u32 = (TAU / ANGLE_RES).ceil() as u32;
     let start = a + Vector2::new(width, 0.0);
     let mut points = vec![start];
-    let rotation = Rotation2::new(2.0 * PI / no_segments as f64);
-    for _ in 0..no_segments {
+    let rotation = Rotation2::new(TAU / segment_count as f64);
+    for _ in 0..segment_count {
         let prev = points.last().unwrap();
         points.push(a + rotation * (prev - a));
     }
@@ -65,15 +67,17 @@ fn circle(a: Point, width: f64) -> Vec<Point> {
         .collect()
 }
 
+
+/// Constructs arc centered in `b` ranging from point `a` to `c` in counterclockwise direction
 fn arc(a: Point, b: Point, c: Point) -> Vec<Point> {
-    let angle = (a - b).angle(&(c - b));
-    if angle < 0.001 {
+    let angle = ccw_angle(&(a - b), &(c - b));
+    if angle < MIN_ANGLE {
         return vec![];
     }
-    let no_segments: u32 = (angle.abs() / ANGLE_RES).ceil() as u32;
-    let rotation = Rotation2::new(angle / no_segments as f64);
+    let segment_count = (angle / ANGLE_RES).ceil() as u32;
+    let rotation = Rotation2::new(angle / segment_count as f64);
     let mut points = vec![a];
-    for _ in 0..(no_segments - 1) {
+    for _ in 0..(segment_count - 1) {
         let prev = points.last().unwrap();
         points.push(b + rotation * (prev - b));
     }
@@ -85,13 +89,65 @@ fn arc(a: Point, b: Point, c: Point) -> Vec<Point> {
         .collect()
 }
 
+/// Given line a -- b -- c constructs a rounded outer corner at point b
 fn elbow(a: Point, b: Point, c: Point, width: f64) -> Vec<Point> {
     let perp_ab = Rotation2::new(FRAC_PI_2) * (b - a).normalize() * width;
     let perp_bc = Rotation2::new(FRAC_PI_2) * (c - b).normalize() * width;
 
-    if (b - a).dot(&perp_bc) > 0.0 {
-        arc(b + perp_ab, b, b + perp_bc)
-    } else {
+    if ccw_turn(a, b, c) {
         arc(b - perp_ab, b, b - perp_bc)
+    } else {
+        arc(b + perp_bc, b, b + perp_ab)
+    }
+}
+
+/// Check if shortest rotation from `from` to `to` is counterclockwise
+fn ccw(from: &Vector, to: &Vector) -> bool { 
+    from.perp(to) > 0.0
+}
+
+/// Check if line `start` -- `through` -- `end` turns counterclockwise
+fn ccw_turn(start: Point, through: Point, end: Point) -> bool {
+    return ccw(&(through - start), &(end - through));
+}
+
+/// Angle from `from` to `to` in counterclockwise direction
+fn ccw_angle(from: &Vector, to: &Vector) -> f64 {
+    if ccw(from, to) {
+        from.angle(to)
+    } else {
+        TAU - from.angle(to)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod ccw {
+        #[test]
+        fn acute_angle_vectors() {
+            let from = Vector::new(-1.0, 2.0);
+            let to = Vector::new(-2.0, 1.0);
+
+            assert!(ccw(from, to));
+            assert!(!ccw(to, from));
+        }
+
+        #[test]
+        fn perpendicular_vectors() {
+            let from = Vector::new(1.0, 0.0);
+            let to = Vector::new(0.0, 1.0);
+
+            assert!(ccw(from, to));
+            assert!(!ccw(to, from));
+        }
+
+        #[test]
+        fn obtuse_angle_vectors() {
+            let from = Vector::new(3.0, 2.0);
+            let to = Vector::new(-2.0, 0.0);
+
+            assert!(ccw(from, to));
+            assert!(!ccw(to, from));
+        }
     }
 }
